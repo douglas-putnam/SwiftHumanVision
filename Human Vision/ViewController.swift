5a718af4bd06940f364d3424f89009f0e67ea693
douglas-putnam <douglasputnam@gmail.com>
2019-01-10 10:56:07 -0500

    Initial Commit

---
+++ Human Vision/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Human Vision
+//
+//  Created by Douglas Putnam on 1/10/19.
+//  Copyright © 2019 Douglas Putnam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
